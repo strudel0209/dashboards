7b695396f62dadb9afd810ce5de9ed90d37a2d50
Alexandra Sachelarescu <9446504+strudel0209@users.noreply.github.com>
2022-09-12 13:37:01 +0300

    Update dashboards.tf

--- dashboards.tf
+++ dashboards.tf
@@ -1,4 +1,4 @@
-resource "grafana_dashboard" "testing" {
+resource "grafana_dashboard" "testing-dash" {
    provider = grafana.cloud
 
    for_each    = fileset("${path.module}/dashboards/testing", "*.json")
