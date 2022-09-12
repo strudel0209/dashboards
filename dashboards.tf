5bfd49ac99b307a0e19f419bcc5123cff695677d
Alexandra Sachelarescu <9446504+strudel0209@users.noreply.github.com>
2022-09-12 14:37:02 +0300

    Update dashboards.tf

--- dashboards.tf
+++ dashboards.tf
@@ -1,7 +1,8 @@
 resource "grafana_dashboard" "testing-dashboards" {
    provider = grafana.cloud
 
-   for_each    = fileset("${path.module}/dashboards/testing", "*.json")
+   #for_each    = fileset("${path.module}/dashboards/testing", "*.json")
+   for_each    = fileset("","${path.module}/dashboards/testing/*.json")
    config_json = file("${path.module}/dashboards/testing/${each.key}")
    folder      = grafana_folder.testing2.id
 }
